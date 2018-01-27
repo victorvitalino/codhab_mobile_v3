1dde1ce9e05c70eadfc2fae64e88e42ed50df5cc
Victor Vitalino <vitalino.victor@gmail.com>
2018-01-27 12:38:21 -0200

    WelcomePass animation text

--- src/pages/welcome/welcome-pass/welcome-pass.ts
+++ src/pages/welcome/welcome-pass/welcome-pass.ts
@@ -7,33 +7,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'welcome-pass.html',
 })
 export class WelcomePassPage {
-
-  private typewriter_text: string = "<h4><p>Sua senha está desatuaizada, informe uma nova senha com 6 números</p>" +
-    "<p>Logo depois repita a mesma senha.</b>.</p></h4>"
-
-  private typewriter_display: string = "";
-
-  private enable_next_button: boolean = false;
-
   private cpf_cnpj: string;
-
   private pass: string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ngOnInit() {
-    this.typingCallback(this);
-  }
-
-  typingCallback(that) {
-    let total_length = that.typewriter_text.length;
-    let current_length = that.typewriter_display.length;
-    if (current_length < total_length) {
-      that.typewriter_display += that.typewriter_text[current_length];
-      setTimeout(that.typingCallback, 40, that);
-    } else {
-      that.enable_next_button = true;
-    }
   }
 
   check_pass(pass){
