6acbe3d0659a0af2f46f167d8797fa70a9f831a5
Victor Vitalino <vitalino.victor@gmail.com>
2018-01-30 19:08:55 -0200

    auth token

--- src/pages/candidate/attendance-prev/attendance-prev.ts
+++ src/pages/candidate/attendance-prev/attendance-prev.ts
@@ -9,21 +9,21 @@ import { AttendanceProvider } from '../../../providers/attendance/attendance';
 })
 export class AttendancePrevPage {
   user_name: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private userService: UserDataProvider,
     private attendanceService: AttendanceProvider) {
   }
 
 
-  ngOnInit() {
-    this.userService.getData().then((resp)=>{
+  ionViewCanEnter(){
+    this.userService.getData().then((resp) => {
       this.user_name = resp.name
+      this.attendanceService.getAttendances(resp.auth).subscribe((resp)=> {
+        
+      })
     });
-    this.attendanceService.getAttendances().subscribe((resp)=>{
-      
-    })
   }
-
   goToAttendanceBasic(){
     this.navCtrl.push("AttendanceBasicPage")
   }
--- src/providers/attendance/attendance.ts
+++ src/providers/attendance/attendance.ts
@@ -1,19 +1,27 @@
 import { HttpModule } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
+import { UserDataProvider } from '../user-data/user-data';
 
 @Injectable()
 export class AttendanceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AttendanceProvider Provider');
+  constructor(public http: Http, private service:UserDataProvider) {
+
   }
 
-  getAttendances(){
-    return this.http.get('/pc/attendance/tickets').map((response:Response) => response)
+  getAttendances(token){
+    let myHeaders = new Headers();
+    myHeaders.set('Content-Type', 'application/json');
+    myHeaders.set('Accept', 'text/plain');
+    myHeaders.set('Authorization',token)
+    console.log(myHeaders)
+
+    return this.http.get('/pc/attendance/tickets',{headers:myHeaders})
+    .map((response:Response) => response)
   }
 }
